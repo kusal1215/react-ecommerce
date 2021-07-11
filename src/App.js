c347b6a64c64e44a96dc7174b3a467b260d1e426
kusal1215 <kusalchathuranga2@gmail.com>
2021-07-11 17:36:50 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Homepage from './pages/homepage.component';
+
+function App() {
+  return (
+    <div className="App">
+        <Homepage/>
+    </div>
+  );
+}
+
+export default App;
